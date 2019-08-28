fb370e72249030eb769c1690a8aad6fab7b9d2c1
lnsdlszsqxxx <lyu4@gmu.edu>
2019-08-28 10:34:49 -0400

    regex

--- src/main/java/mygroup/RegularExpression.java
+++ src/main/java/mygroup/RegularExpression.java
@@ -6,26 +6,21 @@ import java.util.regex.Pattern;
 public class RegularExpression {
 
     public static void main(String[] args) {
-        String str1 = " /depart1ment, /dept,";
-        String str5 = "/department";
-        String str2 = " Derek Banas CA 12345 PA (412)555-1212 johnsmith@hotmail.com 412-555-1234 412 555-1234 ";
-        String str4 = " (703)209-3879 701-201-3871 +1-412-555-1234 412 555-1234 ";
+        String s1="abc acb \\\"abb aabbd aBcd";
+        String s2="bbbb";
+        String s3="bba aba abc ccc, bac   aaaaaaadc www";
+        String regex="(?!(b\\w*))\\w+";
 
-        String str3 = " 1Z aaa **** *** {{{ {{ { ";
-
-//        String regex = "/dep\\w+,";
-        String regex = "/";
-
-        regexChecker(regex,str1);
-
+//        System.out.println(s1.matches("\\w.*"));
+        RegexChecker(regex,s3);
     }
 
-    public static void regexChecker(String regex,String str2Check){
+    public static void RegexChecker(String regex, String str2check){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str2check);
 
-        Pattern checkRegex = Pattern.compile(regex);
-        Matcher matcher = checkRegex.matcher(str2Check);
         while (matcher.find()){
-           if(matcher.group().length()!=0) System.out.println(matcher.group()+" start="+matcher.start()+" end="+matcher.end());
+            if (matcher.group().trim().length()!=0) System.out.println(matcher.group()+" start:"+matcher.start()+" end:"+matcher.end());
         }
     }
 }
